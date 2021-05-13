3b4d9fcb1125edf5873d7dfcea09bd0610d5034e
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-05-13 20:20:41 +0100

    add controller methods, edit edit view and new view, add migration to remove null from bathroom user

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -13,19 +13,34 @@ class BathroomsController < ApplicationController
 
   def create
     @bathroom = Bathroom.new(bathroom_params)
+    # TODO: Complete this action
+    if @bathroom.save
+      redirect_to bathroom_path(@bathroom)
+    else
+      render :new
+    end
   end
 
   def edit
     @bathroom = Bathroom.find(params[:id])
+    # We just need to find the bathroom to populate the form
   end
 
   def update
-
+    @bathroom = Bathroom.find(params[:id])
+    if
+      @bathroom.update(bathroom_params)
+      redirect_to bathroom_path(@bathroom)
+    else
+      render :new
+      # TODO: Complete this action...if etc
+    end
   end
 
   def destroy
     @bathroom = Bathroom.find(params[:id])
     @bathroom.destroy
+    redirect_to bathrooms_path
   end
 
   private
